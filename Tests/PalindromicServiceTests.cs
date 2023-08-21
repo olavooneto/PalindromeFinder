4123d6636398b099d0cf7c79ee7671920cd81cc8
Olavo Oliveira Neto <olavo.o.neto@gmail.com>
2023-08-21 17:06:33 -0300

    initial commit

---
+++ Tests/PalindromicServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using palindromicFinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindromicFinder.Tests
+{
+    [TestClass()]
+    public class PalindromicServiceTests
+    {
+        [TestMethod()]
+        public void FindPalindromicTest01()
+        {
+            var palindomicService = new PalindromicService();
+            var actual = palindomicService.FindPalindromic("abac");
+            var expected = "aba";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindPalindromicTest02()
+        {
+            var palindomicService = new PalindromicService();
+            var actual = palindomicService.FindPalindromic("tacag");
+            var expected = "aca";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindPalindromicTest03()
+        {
+            var palindomicService = new PalindromicService();
+            var actual = palindomicService.FindPalindromic("wegeeksskeegyuwe");
+            var expected = "geeksskeeg";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindPalindromicTest04()
+        {
+            var palindomicService = new PalindromicService();
+            var actual = palindomicService.FindPalindromic("olavo");
+            var expected = "o";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
